4fa8bc5d43f862cda816796c44790a3924c7ab44
Josh Harrison <42451270+Josh-Harrison1600@users.noreply.github.com>
2026-02-01 18:40:49 -0400

    refactored logic to account for all company job postings

--- TokyoDevScraper.py
+++ TokyoDevScraper.py
@@ -5,7 +5,7 @@ HOME_URL = "https://www.tokyodev.com"
 JOBS_URL = "https://www.tokyodev.com/jobs"
 page = requests.get(JOBS_URL)
 
-job_posting_count = 0
+company_count = 0
 
 soup = BeautifulSoup(page.content, "html.parser")
 root_job_element = soup.find("ul", class_="relative list-inside")
@@ -21,47 +21,63 @@ def safety_check_no_output(element):
         print(element.text)
     elif not element:
         return
+    
+def url_safety_check(element, URL):
+    if element:
+        print(URL + element['href'])
+    elif not element:
+        print("No url found for this job")
         
 # List through the job names
 for job_info in root_job_element.find_all("li"):
     
-    job_title = job_info.find("a", class_="hover:text-indigo-600 dark:hover:text-indigo-400")
-    safety_check(job_title, "job title")
+    company_name = job_info.find("a", class_="hover:text-indigo-600 dark:hover:text-indigo-400")
+    safety_check(company_name, "company title")
 
+    print('\n')
+    for i in job_info.find_all("div", class_="relative first:mt-0 mt-4", attrs={"data-collapsable-list-target": "item"}):
         
-    salary_data = job_info.find("a", class_="text-sm tag tag-primary")
-    safety_check(salary_data, "salary data")
-
-    
-    no_japanese_required = job_info.find("a", href="/jobs/no-japanese-required")
-    safety_check_no_output(no_japanese_required)
+        job_title = i.find("h4", class_="text-lg font-bold mb-1")
+        safety_check(job_title, "job title")
         
-    japanese_required = job_info.find("a", href="/jobs/japanese-required")
-    safety_check_no_output(japanese_required)
-    
-    apply_abroad = job_info.find("a", href="/jobs/apply-from-abroad") 
-    safety_check_no_output(apply_abroad)
-    
-    japan_only = job_info.find("a", href="/jobs/residents-only")
-    safety_check_no_output(japan_only)
-    
-    full_remote = job_info.find("a", href="/jobs/fully-remote")
-    safety_check_no_output(full_remote)
-    
-    partially_remote = job_info.find("a", href="/jobs/partially-remote")
-    safety_check_no_output(partially_remote)
+        job_url = i.find("a", class_="font-bold hover:text-indigo-600 dark:hover:text-indigo-400", href=True)
+        url_safety_check(job_url, HOME_URL)
+        
+        salary_data = i.find("a", class_="text-sm tag tag-primary")
+        safety_check(salary_data, "salary data")
 
-    no_remote = job_info.find("a", href="/jobs/no-remote")
-    safety_check_no_output(no_remote)
+        no_japanese_required = i.find("a", href="/jobs/no-japanese-required")
+        safety_check_no_output(no_japanese_required)
+            
+        japanese_required = i.find("a", href="/jobs/japanese-required")
+        safety_check_no_output(japanese_required)
+        
+        apply_abroad = i.find("a", href="/jobs/apply-from-abroad") 
+        safety_check_no_output(apply_abroad)
+        
+        japan_only = i.find("a", href="/jobs/residents-only")
+        safety_check_no_output(japan_only)
+        
+        full_remote = i.find("a", href="/jobs/fully-remote")
+        safety_check_no_output(full_remote)
+        
+        partially_remote = i.find("a", href="/jobs/partially-remote")
+        safety_check_no_output(partially_remote)
+
+        no_remote = i.find("a", href="/jobs/no-remote")
+        safety_check_no_output(no_remote)
+        print('\n')
+
     
+    # Get all the job links from the company
     print('\n'"Job postings for this company:")
-    for i in job_info.find_all("a", class_="font-bold hover:text-indigo-600 dark:hover:text-indigo-400", href=True):    
-        print("    ", HOME_URL + i['href'])
+    for j in job_info.find_all("a", class_="font-bold hover:text-indigo-600 dark:hover:text-indigo-400", href=True):    
+        print("    ", HOME_URL + j['href'])
     
 
-    job_posting_count += 1
+    company_count += 1
     print(" ")
-    print("---------------------- Job ", job_posting_count, "----------------------")
+    print("---------------------- Company ", company_count, "----------------------")
     print(" ")
     
-print("counted ", job_posting_count, " jobs")
+print("counted ", company_count, " companies")
