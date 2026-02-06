792dd3542d3fd5dd9987241bbd988b3231a19f45
Josh Harrison <42451270+Josh-Harrison1600@users.noreply.github.com>
2026-02-05 21:26:49 -0400

    fixed import path

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from scrapers.JapanDevScraper import japan_dev_scraper
 from scrapers.TokyoDevScraper import tokyo_dev_scraper
-from SendEmail import send_email
+from emailer.SendEmail import send_email
 
 japan_dev_jobs = japan_dev_scraper()
 tokyo_dev_jobs = tokyo_dev_scraper()
